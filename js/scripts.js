f5a042383fb100ac42fc446d82b3e0e5a3843401
Krzysztof Barański <Krzysztof Baraski>
2017-05-01 21:55:42 +0200

    poprawione wciecia i klasy

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 $('#search').click(searchCountries);
+
 function searchCountries() {
- 	var countryName = $('#country-name').val();
-  var countryCapital = $('#country-capital').val();
-if(!countryName.length) countryName = 'Poland';
-$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+	var countryName = $('#country-name').val();
+	var countryCapital = $('#country-capital').val();
+	if (!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
 }
+
 function showCountriesList(resp) {
-  	countriesList.empty();
-resp.forEach(function(item) {	 
-  $('<li class="Kraj">').text('Kraj: ' + item.name + ', Stolica: ' + item.capital).appendTo(countriesList);
-   $('<ul class="Granice"> <li>').text('Graniczy z: ' 
-                   + item.borders).appendTo(countriesList);
-   $('<ul class="Jezyki"> <li>').text('U¿ywane jêzyki: ' + item.languages).appendTo(countriesList);
-  $('<ul class="Waluta"> <li>').text('Waluta: ' + item.currencies).appendTo(countriesList);
-});
+	countriesList.empty();
+	resp.forEach(function (item) {
+		$('<li class="country">').text('Kraj: ' + item.name + ', Stolica: ' + item.capital).appendTo(countriesList);
+		$('<ul class="borders"> <li>').text('Graniczy z: ' +
+			item.borders).appendTo(countriesList);
+		$('<ul class="languages"> <li>').text('U¿ywane jêzyki: ' + item.languages).appendTo(countriesList);
+		$('<ul class="currency"> <li>').text('Waluta: ' + item.currencies).appendTo(countriesList);
+	});
 }
